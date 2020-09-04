46bbb06f238da97bf4dfb50b45d5d5a7172d8d66
shah2s7 <shah2s7@mail.uc.edu>
2020-09-04 16:27:22 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,39 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("M&M Color Counts");
+    {
+      int total;
+      total = 55*10;
+      double blue;
+      blue = total * .24;
+      double brown;
+      brown = total * .13;
+      double green;
+      green = total * .16;
+      double orange;
+      orange = total * .20;
+      double red;
+      red = total * .13;
+      double yellow;
+      yellow = total * .14;
+    
+      System.out.println("blue: " + blue);
+      System.out.println("brown: " + brown);
+      System.out.println("green: " + green);
+      System.out.println("orange: " + orange);
+      System.out.println("red: " + red);
+      System.out.println("yellow: " + yellow);
+  
+      double sum;
+      sum = blue + brown + green + orange + red + yellow;
+      System.out.println("Sum: " + sum);
+
+      if (blue > brown && green > orange)
+      System.out.println("Blue over Brown and Green over Orange");
+      if (brown <= red)
+      System.out.println("Brown is less than or equal to Red");
+      if (sum == total)
+      System.out.println("Numbers match");
+    }
+  }
+}
